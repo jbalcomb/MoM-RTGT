cced85c4e658203172d446ad026c1d23766e973c
I like Serena (Linux) <klaas.vanaarsen@xs4all.nl>
2012-03-18 23:22:33 +0100

    Started a test framework MoMTest

---
+++ MoMEditorTemplate/QMoMTreeItem.cpp
@@ -0,0 +1,10 @@
+// ---------------------------------------------------------------------------
+// Copyright:   2011 Klaas van Aarsen
+// License:     GPL
+// Author:      I like Serena (aka Klaas van Aarsen)
+// Created:     2011-09-30
+// ---------------------------------------------------------------------------
+
+#include "QMoMTreeItem.h"
+
+MoM::MoMGameBase* QMoMTreeItemBase::m_game = 0;
